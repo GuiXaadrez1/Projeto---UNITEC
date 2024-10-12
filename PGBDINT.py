63b6cd33912a35d8718dd7ba318a7fb3c0b5c873
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2024-10-12 15:23:21 -0300

    Integração banco

---
+++ PGBDINT.py
@@ -0,0 +1,42 @@
+import psycopg2
+import pprint
+
+def conexao():
+    try:
+        connection = psycopg2.connect(
+            user="postgres",
+            password="32481024",
+            host="localhost",
+            port="5432",
+            database="unitec"
+        )
+        
+        cursor = connection.cursor()
+        print("Conexão com PostgreSQL estabelecida com sucesso")
+
+        # Executar uma consulta
+        cursor.execute("SELECT * FROM pessoa")
+        records = cursor.fetchall()  # Pega todas as linhas da tabela
+
+        # Pega o nome das colunas da tabela
+        colunas = [desc[0] for desc in cursor.description]
+
+        # Exibe o cabeçalho da tabela
+        print(f"{' | '.join(colunas)}")
+        print('-' * 50)  # Uma linha separadora
+
+        # Exibe os registros formatados
+        for record in records:
+            print(" | ".join(map(str, record)))
+
+    except (Exception, psycopg2.Error) as error:
+        print("Erro ao conectar ao PostgreSQL", error)
+
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Conexão com PostgreSQL fechada")
+
+# Chama a função
+conexao()
