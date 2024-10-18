4dda4940568dda52d895a365347af54e23968950
GHAS06 <guilherme.silva@cleversystems.net>
2024-10-18 14:43:14 -0300

    Adcionando rota raiz_arquivo_principal

--- unitec_main.py
+++ unitec_main.py
@@ -9,6 +9,7 @@ from flask import render_template, request, jsonify
 from BD.database import conf_conexao
 
 # Importando diretamente na pasta routes as variáveis que comportam as blueprints!
+from routes.raiz import raiz_route
 from routes.login import login_route
 from routes.recuperar_pw import recuperar_pw_route
 from routes.home  import home_route
@@ -19,12 +20,13 @@ app = Flask(__name__)
 mydb = conf_conexao()
 
 # @ é um decorador que define as rotas (caminhos na URL) da aplicação.
+'''
 @app.route('/')
 def raiz():
     return '<h1><strong>Você está na raiz<strong><h1>'
-
-# Registrando os Blueprint no arquivo principal
-
+'''
+# Registrando os Blueprint no arquivo principal, vamos usar muito no projeto!
+app.register_blueprint(raiz_route)
 app.register_blueprint(login_route)
 app.register_blueprint(recuperar_pw_route)
 app.register_blueprint(home_route)
