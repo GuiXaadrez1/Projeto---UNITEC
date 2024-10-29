71b844fc3a8d45e05e0b2510ff0be65c5943d654
GuiXaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2024-10-29 09:44:05 -0300

    Adcionando rota para painel do admin

--- routes/admin.py
+++ routes/admin.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template, redirect, url_for
 
 admin_route = Blueprint('admin',__name__)
 
+@admin_route.route('/')
+def admin_raiz():
+     return "Tela_painel_admin"
+     
 @admin_route.route('/cadastrar_pessoa')
 def cadastro_pessoa():
      return render_template('cadastro_pessoa.html')
